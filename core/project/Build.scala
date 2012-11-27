3dd50f375806276e195cc5c77d4f14c7898386bf
Kahli Burke <kahli.burke@wintermutesys.com>
2012-11-27 12:17:53 -0800

    Provide working example of submodule routes
    Also ran into an issue with pulling in the submodules as jars, in the
    Build.scala (core) file you can change to pulling in as SBT dependency
    and that seems to fix the issue

--- core/project/Build.scala
+++ core/project/Build.scala
@@ -7,14 +7,17 @@ object ApplicationBuild extends Build {
   val appName = "core"
   val appVersion = "1.0-SNAPSHOT"
 
+  // Bringing in the libraries these ways will work for the submodules, but the core routes hang?!
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm,
-    "submodule2" % "submodule2_2.10" % "1.0-SNAPSHOT",
-    "submodule1" % "submodule1_2.10" % "1.0-SNAPSHOT")
+    //"submodule2" %% "submodule2" % "1.0-SNAPSHOT",
+    "submodule1" %% "submodule1" % "1.0-SNAPSHOT")
 
+
+  // If I comment out the submodules above and bring in the submodule from underneath the modules directory (SBT constrains them to be in a subdir)
+  // then all the routes work.
+  //val sub1 = play.Project("sub1", appVersion, Nil, path=file("modules/submodule1"))
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
-  )
+  )//.dependsOn(sub1)
 
 }
